31782145cc2e1b8908bacb8f8bcffc00951ca62d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-11-07 16:26:51 +0100

    Obsługa zdarzeń

---
+++ src/WpfClient/Behaviors/ButtonBehavior.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfClient.Behaviors
+{
+    internal class ButtonBehavior : Behavior<Button>
+    {
+        public string Color
+        {
+            get { return (string)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Color.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register("Color", typeof(string), typeof(ButtonBehavior), new PropertyMetadata(string.Empty));
+
+
+
+        // public string Color { get; set; }
+
+        protected override void OnAttached()
+        {
+            this.AssociatedObject.MouseEnter += AssociatedObject_MouseEnter;
+        }
+
+        private void AssociatedObject_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                button.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(Color));
+            }
+
+            
+        }
+    }
+}
---
+++ src/WpfClient/MarkupExtensions/MyExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace WpfClient.MarkupExtensions
+{
+    // https://thomaslevesque.com/2011/09/23/wpf-4-5-subscribing-to-an-event-using-a-markup-extension/
+    internal class MyExtension : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var provideValueTarget = (IProvideValueTarget)serviceProvider.GetService(typeof(IProvideValueTarget));
+            throw new NotImplementedException();
+        }
+    }
+}
