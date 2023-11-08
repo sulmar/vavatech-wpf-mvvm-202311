d5b47ca9c4e72c191aa6c02ae358b6c791fc1ed1
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-11-08 14:45:09 +0100

    IValueConverter

---
+++ src/WpfClient/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfClient.Converters;
+
+internal class BooleanToVisibilityConverter : IValueConverter
+{
+    public Visibility FalseValue { get; set; } = Visibility.Collapsed;
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue)
+        {
+            return boolValue ? Visibility.Visible : FalseValue;
+        }
+
+        throw new InvalidCastException();
+
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
