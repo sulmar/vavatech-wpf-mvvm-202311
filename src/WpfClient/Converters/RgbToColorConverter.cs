a109d0e9d2cbd8fb2331e2f19ac1901fa7f92f65
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-11-08 14:47:03 +0100

    Create RgbToColorConverter.cs

---
+++ src/WpfClient/Converters/RgbToColorConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfClient.Converters;
+
+class RgbToColorConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
